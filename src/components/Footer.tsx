932640de4119261133ac0410f4577d79a19e99c4
BlueWave <117451172+KaveeGitHub@users.noreply.github.com>
2026-01-05 15:32:32 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Bot, Mail, MapPin, Phone, Linkedin, Twitter, Instagram } from "lucide-react";
-import logo from "src/components/logo.png";
+import logo from "logo.png";
 
 
 const Footer = () => {
