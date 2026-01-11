361612ceb69512bad0741458d8d53568479b9ffc
BlueWave <117451172+KaveeGitHub@users.noreply.github.com>
2026-01-11 20:10:47 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ import logo from "./logo.png";
 
 const Footer = () => {
   return (
+    <section id="contacts">
     <footer className="bg-card border-t border-border">
       <div className="container mx-auto px-4 py-16">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
